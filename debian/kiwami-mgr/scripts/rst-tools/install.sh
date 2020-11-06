884c099e6ae84e80b25f91d57b301cd1c1feb860
Chua Hou <human+github@chuahou.dev>
2020-11-07 00:24:39 +0800

    kiwami-mgr: remove Sphinx and breathe

--- debian/kiwami-mgr/scripts/rst-tools/install.sh
+++ debian/kiwami-mgr/scripts/rst-tools/install.sh
@@ -12,7 +12,3 @@ pip3 install https://github.com/Rykka/instant-rst.py/archive/master.zip
 # install vim-rst-tables
 # https://github.com/ossobv/vim-rst-tables-py3
 pip3 install vim_bridge3
-
-# install Sphinx and read the docs theme
-# install breathe for doxygen interface
-pip3 install sphinx_rtd_theme breathe
