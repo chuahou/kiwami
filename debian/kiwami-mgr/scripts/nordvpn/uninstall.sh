dfe37c4af154ead880017dfadf0844496f56b246
Chua Hou <human+github@chuahou.dev>
2020-11-09 03:26:52 +0800

    kiwami-mgr, init.d: remove nordvpn from mgr and add to init.d

--- debian/kiwami-mgr/scripts/nordvpn/uninstall.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# SPDX-License-Identifier: MIT
-# Copyright (c) 2020 Chua Hou
-
-check-root root || exit 1
-
-apt-get purge nordvpn-release nordvpn
--- init.d/nordvpn.sh
+++ init.d/nordvpn.sh
@@ -2,10 +2,8 @@
 # SPDX-License-Identifier: MIT
 # Copyright (c) 2020 Chua Hou
 
-check-root root || exit 1
-
 wget https://repo.nordvpn.com/deb/nordvpn/debian/pool/main/nordvpn-release_1.0.0_all.deb
-dpkg -i nordvpn-release_1.0.0_all.deb
+sudo dpkg -i nordvpn-release_1.0.0_all.deb
 rm nordvpn-release_1.0.0_all.deb
-apt-get update
-apt-get install nordvpn -y
+sudo apt-get update
+sudo apt-get install nordvpn -y
