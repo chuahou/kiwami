faa857b1e3f11cc5c4fa7178e2605e0f4cbe52ce
Chua Hou <human+github@chuahou.dev>
2020-11-10 13:30:49 +0800

    init.d: remove nordvpn
    * Added to kiwami-apt-source

--- init.d/nordvpn.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# SPDX-License-Identifier: MIT
-# Copyright (c) 2020 Chua Hou
-
-wget https://repo.nordvpn.com/deb/nordvpn/debian/pool/main/nordvpn-release_1.0.0_all.deb
-sudo dpkg -i nordvpn-release_1.0.0_all.deb
-rm nordvpn-release_1.0.0_all.deb
-sudo apt-get update
-sudo apt-get install nordvpn -y
